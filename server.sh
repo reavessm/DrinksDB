8b476573cdc1b66bef0ffe0c238fe070a2cec7ed
Stephen Reaves <reavessm@email.sc.edu>
2018-07-25 14:02:52 -0400

    :rocket: Server no longer creates index.html

--- server.sh
+++ server.sh
@@ -2,14 +2,14 @@
 if [[ -f drinks.db ]]
 then
   STUFF="$(sqlite3 drinks.db "select * from Drinks;" \
-    | pandoc -s - -H meta.html -H style.html -o index.html && cat index.html)"
+    | pandoc -s - -H meta.html -H style.html -t html)"
   RESPONSE="HTTP/1.1 200 OK\r\nConnection: keep-alive\r\n\r\n$STUFF\r\n"
 fi
 while { echo -en "$RESPONSE"; } | nc -l "${1:-8080}"; do
   if [[ -f drinks.db ]]
   then
     STUFF="$(sqlite3 drinks.db "select * from Drinks;" \
-      | pandoc -s - -H meta.html -H style.html -o index.html && cat index.html)"
+      | pandoc -s - -H meta.html -H style.html -t html)"
     RESPONSE="HTTP/1.1 200 OK\r\nConnection: keep-alive\r\n\r\n$STUFF\r\n"
   fi
 done
